c4991f0da8a135601657c703cca2565021ab2f28
Lauriero <incorobots@gmail.com>
2022-01-08 12:49:37 +0300

    Initial commit

---
+++ example/lcc_api_dart_example.dart
@@ -0,0 +1,6 @@
+import 'package:lcc_api_dart/lcc_api_dart.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/lcc_api_dart.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library lcc_api_dart;
+
+export 'src/lcc_api_dart_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
